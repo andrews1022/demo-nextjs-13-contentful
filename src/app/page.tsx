a851329c232773abf61832f01b05c39e1115f919
Andrew Shearer <andrewshearerdev@gmail.com>
2023-10-25 18:00:22 -0700

    added example of fetching with graphql

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ type ProjectSkeleton = {
   };
 };
 
-const getProjects = async () => {
+const getProjectsRest = async () => {
   const entries = await client.getEntries<ProjectSkeleton>({
     content_type: "project"
   });
@@ -20,16 +20,55 @@ const getProjects = async () => {
   return entries;
 };
 
+const gql = String.raw;
+
+const getProjctsGraphql = async () => {
+  const url = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`;
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.CONTENTFUL_CONTENT_DELIVERY_API_ACCESS_TOKEN}`
+    },
+    body: JSON.stringify({
+      query: gql`
+        query HomepageQuery {
+          projectCollection {
+            items {
+              sys {
+                id
+              }
+              title
+            }
+          }
+        }
+      `
+    })
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch from Contentful");
+  }
+
+  const data = await res.json();
+  console.log("data: ", data);
+
+  return data;
+};
+
 const HomePage = async () => {
-  const projects = await getProjects();
+  const projectsRest = await getProjectsRest();
+
+  const projectsGraphql = await getProjctsGraphql();
 
   return (
     <div>
       <h1>HomePage</h1>
 
-      {projects.items.map((project) => (
+      {/* {projects.items.map((project) => (
         <div key={project.sys.id}>{project.fields.title}</div>
-      ))}
+      ))} */}
     </div>
   );
 };

da4169e8b8491c7dd5e351ad4f266755970c411f
Andrew Shearer <andrewshearerdev@gmail.com>
2023-10-25 17:37:36 -0700

    updated home page to display project names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
-const HomePage = () => {
+import { client } from "@/contentful/client";
+import type { EntryFieldTypes } from "contentful";
+
+type ProjectSkeleton = {
+  contentTypeId: "project";
+  fields: {
+    title: EntryFieldTypes.Text;
+  };
+};
+
+const getProjects = async () => {
+  const entries = await client.getEntries<ProjectSkeleton>({
+    content_type: "project"
+  });
+
+  if (!entries) {
+    throw new Error("No entries found");
+  }
+
+  return entries;
+};
+
+const HomePage = async () => {
+  const projects = await getProjects();
+
   return (
     <div>
       <h1>HomePage</h1>
+
+      {projects.items.map((project) => (
+        <div key={project.sys.id}>{project.fields.title}</div>
+      ))}
     </div>
   );
 };
